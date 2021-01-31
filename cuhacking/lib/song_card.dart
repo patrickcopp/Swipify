ae422d5eaecd8cf4bba8a874f818f343c84d021c
Gerudhoh <hohenade@uoguelph.ca>
2021-01-30 22:45:38 -0500

    Making songcards stateful

--- cuhacking/lib/song_card.dart
+++ cuhacking/lib/song_card.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
 
-class SongCard extends StatelessWidget {
+class SongCard extends StatefulWidget {
   const SongCard(
-      {Key key,
-      this.color = Colors.indigo,
-      this.trackTitle = "Card Example",
-      this.imageUrl = "none",
-      this.URI = ""})
-      : super(key: key);
+  {Key key,
+  this.color = Colors.indigo,
+  this.trackTitle = "Card Example",
+  this.imageUrl = "none",
+  this.URI = ""})
+  : super(key: key);
   final Color color;
   final String trackTitle;
   final String imageUrl;
   final String URI;
 
+  @override
+  _SongCardState createState() => _SongCardState();
+}
+
+class _SongCardState extends State<SongCard> {
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -22,7 +27,7 @@ class SongCard extends StatelessWidget {
       // Warning: hard-coding values like this is a bad practice
       padding: EdgeInsets.all(38.0),
       decoration: BoxDecoration(
-        color: color,
+        color: this.widget.color,
         borderRadius: BorderRadius.circular(15.0),
         border: Border.all(
           width: 7.0,
@@ -32,7 +37,7 @@ class SongCard extends StatelessWidget {
 
       child: new Column(children: [
         Text(
-          trackTitle,
+          this.widget.trackTitle,
           style: TextStyle(
             fontSize: 18.0,
             // color: Colors.white,
@@ -40,7 +45,7 @@ class SongCard extends StatelessWidget {
             fontWeight: FontWeight.w900,
           ),
         ),
-        new Image.network(imageUrl)
+        new Image.network(this.widget.imageUrl)
       ]),
     );
   }
